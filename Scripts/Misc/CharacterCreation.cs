01a034f167457c36bc5a89adf5f2a0482c23fcbe
zeeb <zeeb1337@hotmail.com>
2023-11-30 22:07:55 +0100

    Changed expansion
    Switched expansion from Endless Journey to Time of Legends and removed starting gold from players.

--- Scripts/Misc/CharacterCreation.cs
+++ Scripts/Misc/CharacterCreation.cs
@@ -62,7 +62,7 @@ namespace Server.Misc
 			}
 
 			PackItem(new RedBook("a book", m.Name, 20, true));
-			PackItem(new Gold(1000)); // Starting gold can be customized here
+			//PackItem(new Gold(1000)); // Starting gold can be customized here
 			PackItem(new Candle());
 
 			if (m.Race != Race.Gargoyle)
